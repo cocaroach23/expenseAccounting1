b0eac9d74a51ea69a65ca9ff4fef569860f56c19
cocaroach23 <cocaroach23@gmail.com>
2024-01-09 09:48:37 +0300

    правка

--- src/components/GeneralResults/index.jsx
+++ src/components/GeneralResults/index.jsx
@@ -8,9 +8,9 @@ const generalResults = ({newPurchase}) => {
     
     const generalExpenses = newPurchase.reduce((acc , newPurchase, i , arr) => {
         console.log(acc);
-        return acc + oldPurchase.cash;
+        return acc + newPurchase.cash;
 }, 0);
-    console.log(generalExpenses);
+    console.log(generalExpenses); // не очень понимаю какой массив нужно брать старый или новый
     
 
     return (
